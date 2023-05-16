89e3e3626839d0fe02107cfb138a2155a032cc60
Colin Spear <colinspear@gmail.com>
2023-05-16 09:41:14 -0700

    added an idea for updating content automatically in deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,17 @@
 # If a command fails then the deploy stops
 set -e
 
+## get new content from notes repo
+#cd content
+#git pull
+#
+## update website repo
+#cd ..
+#git add .
+#git commit -m 'updating website'
+#git push
+
+# update website
 printf "\033[0;32mTearing down and rebuilding site...\033[0m\n"
 
 rm -r public
